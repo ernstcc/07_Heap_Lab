78047da1a08eb686e9a2e8bc7924d73a9765cb53
ernstcc <ernstcc@miamioh.edu>
2014-11-12 00:13:04 -0500

    Completed all functions except for remove

--- Heap.h
+++ Heap.h
@@ -45,5 +45,68 @@ private:
   // place
   void trickleDown(unsigned long index);  
 };
+#include <string>
 
-#include "Heap.ipp"
+template<class Pri, class T>
+Heap<Pri, T>::Heap(){
+	arrSize = START_SIZE;
+	numItems = 0;
+	backingArray = new std::pair<Pri,T>[START_SIZE];
+}
+
+template<class Pri, class T>
+Heap<Pri, T>::~Heap(){
+	//TODO
+}
+
+template<class Pri, class T>
+void Heap<Pri, T>::grow(){
+	int newArr = arrSize * 2;
+	std::pair<Pri, T>* temp = new std::pair<Pri, T>[newArr];
+
+	for (int x = 0; x < arrSize; x++){
+		temp[x] = backingArray[x];
+	}
+
+	delete[] backingArray;
+	backingArray = temp;
+	arrSize = newArr;
+}
+
+template<class Pri, class T>
+void Heap<Pri, T>::add(std::pair<Pri, T> toAdd){
+	if (numItems == arrSize){
+		grow();
+	}
+
+	backingArray[numItems] = toAdd;
+	numItems++;
+	bubbleUp(numItems);
+}
+
+template<class Pri, class T>
+void Heap<Pri, T>::bubbleUp(unsigned long index){
+	unsigned long p = parent(index);
+	while (index > 0 && backingArray[index] < backingArray[p]){
+		swap(backingArray[index], backingArray[p])'
+		index = p;
+		p = parent(index);
+	}
+}
+
+template<class Pri, class T>
+void Heap<Pri, T>::trickleDown(unsigned long index){
+	
+}
+
+template<class Pri, class T>
+std::pair<Pri, T> Heap<Pri, T>::remove(){
+	//TODO
+	std::pair<Pri, T> tmp;
+	return tmp;
+}
+
+template<class Pri, class T>
+unsigned long Heap<Pri, T>::getNumItems(){
+	return numItems;
+}
